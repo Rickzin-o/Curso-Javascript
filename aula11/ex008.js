2d44a9248e94631f12b18945674b628339c1f058
José Ricardo <ricardoanimation2@gmail.com>
2023-11-04 15:51:49 -0300

    Aula 11 concluída

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var velocity = 78
+console.log(`A velocidade do seu carro é ${velocity}km/h`)
+
+if (velocity > 60) {
+    console.log("Você ultrapassou o limite de velocidade permitida, MULTADO!")
+}
+
+console.log("Dirija sempre usando cinto de segurança!")
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = "Espanha"
+console.log(`Se você vive em ${país}`)
+
+if (país == "Brasil") {
+    console.log("Você é brasileiro")
+} else {
+    console.log("Você é estrangeiro!")
+}
